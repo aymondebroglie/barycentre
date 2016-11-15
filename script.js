15569fd7fb7a469cf066d30329ed1e8424969757
Aymon de Broglie <aymondebroglie@gmail.com>
2016-11-15 20:58:07 +0100

    Préparation pour calculer les temps de trajet

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ function distance(point_a,point_b){
 	return Math.pow(point_a.lat() - point_b.lat(),2) + Math.pow(point_a.lng() - point_b.lng(),2); 
 }
 
+// Cette fonction permet de calculer un barycentre elle prend en parametre un tableau de latitude, de longitude, le nbr d'amis et les poids à attribuer
 function calcul_bary(lat_array,long_array,nbr_amis,poids){
 	//Calcule du barycentre
     	var lat_bary = 0;
@@ -94,7 +95,26 @@ function calcul_bary(lat_array,long_array,nbr_amis,poids){
     	var origin = calcul_bary(lat_array,long_array,nbr_amis,poids);
 		var latlng_bary = origin;
  
-    	
+ //Ici on va calculer les temps de trajet 
+ 		function callback(response, status) {
+  			if (status == google.maps.DistanceMatrixStatus.OK) {
+    			var origins = response.originAddresses;
+    			var destinations = response.destinationAddresses;
+
+    			for (var i = 0; i < origins.length; i++) {
+      				var results = response.rows[i].elements;
+      				for (var j = 0; j < results.length; j++) {
+        				var element = results[j];
+        				var distance = element.distance.text;
+        				var duration = element.duration.text;
+        				var from = origins[i];
+        				var to = destinations[j];
+      				}
+    			}
+  			}
+		}
+
+
 
 // On cherche la station de métro la plus proche si elle existe 
 	
