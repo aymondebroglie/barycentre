452cbf89846d34050d6b9ef686816f15c4d0264b
Aymon de Broglie <aymondebroglie@gmail.com>
2016-11-18 13:37:13 +0100

    Préparation pour le calcul du barycentre amélioré

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ function distance(point_a,point_b){
 	return Math.pow(point_a.lat() - point_b.lat(),2) + Math.pow(point_a.lng() - point_b.lng(),2); 
 }
 
+//Cette fonction enlève les markers du tableau de lamap 
 function clean(tab_markers){
 	for (var i = 0; i < tab_markers.length; i++) {
     tab_markers[i].setMap(null);
@@ -28,7 +29,7 @@ function clean(tab_markers){
 }
 
 // Cette fonction permet de calculer un barycentre elle prend en parametre un tableau de latitude, de longitude, le nbr d'amis et les poids à attribuer
-function calcul_bary(users_array,nbr_amis,poids){
+function calcul_bary(users_array,poids){
 	//Calcule du barycentre
     	var lat_bary = 0;
     	var long_bary = 0;
@@ -38,24 +39,38 @@ function calcul_bary(users_array,nbr_amis,poids){
     		lat_bary = lat_bary + pd_temp*users_array[i].lat();
     		long_bary = long_bary + pd_temp*users_array[i].lng();
     	};
-    	
-    	lat_bary = lat_bary/nbr_amis;
-    	long_bary = long_bary/nbr_amis;
+    	var sum = somme(poids);
+    	lat_bary = lat_bary/sum;
+    	long_bary = long_bary/sum;
     	var origin = new google.maps.LatLng(lat_bary,long_bary);
     	return origin;
+}
+
+//Cette fonction calcule la moyenne d'un tableau
+function moyenne(tab){
+	var mean=0;
+	for(var i = 0; i < tab.length ; i++){
+		mean = mean + tab[i];
+	}
+	return mean/tab.length;
+}
+//Cette fonction retourne la somme d'un tableau 
+
+function somme(tab){
+	var sum = 0;
+	for (var i = 0; i < tab.length; i++){
+		sum = sum + tab[i];
+	}
+	return sum;
 }
 
 //Cette fonction test si le tableau des temps de trajets est suffisamment lisse
 function test_tableau(tab){
 	//On calcule la moyenne
-	var moyenne=0;
+	
+	mean = moyenne(tab);
 	for(var i = 0; i < tab.length ; i++){
-		moyenne = moyenne + tab[i];
-	}
-	moyenne = moyenne/tab.length;
-
-	for(var i = 0; i < tab.length ; i++){
-		if(tab[i] > 1.2*moyenne ){
+		if(tab[i] > 1.2*mean ){
 			return false
 		}
 	}
@@ -63,7 +78,22 @@ function test_tableau(tab){
 }
 
 
+//Cette fonction v, à partir du tableau des temps de trajet,calculer un nouveau tableau de poids 
 
+function calcul_poids(tab){
+		var poids = [];
+		mean = moyenne(tab);
+	for(var i = 0; i < tab.length ; i++){
+		if(tab[i] > 1.2*mean ){
+			poids.push(1 + (tab[i] -mean)/mean)
+		}
+		else{
+			poids.push(1);
+		}
+
+	}
+	return poids;
+}
 
 // Si on clique sur c'est parti affiche un formulaire par participant
     $('#parti').click(function(){
@@ -121,9 +151,9 @@ function test_tableau(tab){
 
     	
     	
-    	var origin = calcul_bary(users_array,nbr_amis,poids);
-		var latlng_bary = origin;
- 
+    	var latlng_bary = calcul_bary(users_array,poids);
+		//Evite les boucles infinies si erreur de code 
+ 		var compteur = 0;
 		//
 
  //Ici on va calculer les temps de trajet 
@@ -131,6 +161,7 @@ function test_tableau(tab){
 		
  
 		var service = new google.maps.DistanceMatrixService();
+		
 		service.getDistanceMatrix(
   			{
     			origins: users_array,
@@ -140,6 +171,8 @@ function test_tableau(tab){
 
 
  		function callback(response, status) {
+ 			//On récupère le tableau des temps de trajet 
+ 			compteur = compteur +1 ;
   			if (status == google.maps.DistanceMatrixStatus.OK) {
   				console.log(response);
   				var temps_trajet = [];
@@ -152,17 +185,29 @@ function test_tableau(tab){
         				var element = results[j];        				
         				var duration = element.duration.value;
         				temps_trajet.push(duration);
-        				console.log(temps_trajet);
 		      				}
     			}
+    			console.log(temps_trajet);
+    			console.log(test_tableau(temps_trajet));
+    			if(!test_tableau(temps_trajet)){
+    			//Dans ce cas il faut caluler un nouveau barycentre 
+    			//On commence par calculer le nouveau tableau de poids  
+    			poids = calcul_poids(temps_trajet);
+    			console.log(poids);
+    			console.log(latlng_bary.lat());
+    			console.log(latlng_bary.lng());
+    			//On calcul le nouveau barycentre 
+    			latlng_bary = calcul_bary(users_array,poids);
+
+    			console.log(latlng_bary.lat());
+    			console.log(latlng_bary.lng());
+    			console.log("salut");
+    			}
+ 
+				    		
   			}
 
-
-		}
-
-
-
-// On cherche la station de métro la plus proche si elle existe 
+  			// On cherche la station de métro la plus proche si elle existe 
 
 	
 		var request = {
@@ -181,7 +226,7 @@ function test_tableau(tab){
     			var temp;
     			for(var i =0; i < results.length ; i++){
     				temp = results[i].geometry.location;
-    				var distance_temp = distance(origin,temp);
+    				var distance_temp = distance(latlng_bary,temp);
 
     				if( distance_temp < dist_min){
     					latlng_bary = temp;
@@ -204,6 +249,11 @@ function test_tableau(tab){
      		tab_markers.push(marker);
     }
   );
+		}
+
+
+
+
 
 
 
